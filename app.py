ecd09f83f6e84c182c0a0920fa04424f6cd90113
Mateusz Miroslaw Lis <78234473+supergeniodelmale@users.noreply.github.com>
2024-02-29 16:36:55 +0100

    Update and rename gradiofilter.py to app.py

--- app.py
+++ app.py
@@ -2,27 +2,21 @@ import gradio as gr
 from df.enhance import enhance, init_df, load_audio, save_audio
 from df.utils import download_file
 
-# Define the transformation function
 def transform_audio(audio_file):
     print(audio_file)
-    # Load default model
     model, df_state, _ = init_df()
     audio, _ = load_audio(audio_file, sr=df_state.sr())
-    # Denoise the audio
     enhanced = enhance(model, df_state, audio)
-    # Save for listening
     save_audio(audio_file.replace(".wav","_output.wav"), enhanced, df_state.sr())
-    #audio = load_audio(audio_file.replace(".wav","_output.wav"), sr=df_state.sr())
     return audio_file.replace(".wav","_output.wav")
 
-# Define the UI
 gr.Interface(
     fn=transform_audio,
     inputs=[
         gr.Audio(sources="upload", type="filepath", label="Upload Audio File")
     ],
-    outputs=gr.Audio(label="Transformed Audio"),
-    title="Audio Transformation",
-    description="Upload an audio file and apply a transformation.",
+    outputs=gr.Audio(label="Denoised audio"),
+    title="DeepFilterNet Web UI",
+    description="A simple web UI built in Gradio for audio denoising with DeepFilterNet.",
     live='true'
 ).launch(inbrowser='true')
